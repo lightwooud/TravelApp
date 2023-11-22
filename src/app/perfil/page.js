22ae110c0f66c4902ce68d6e75c7914368138f00
lightwood <sebastiandias1999@gmail.com>
2023-11-22 08:25:39 -0500

    ahora 2

--- src/app/perfil/page.js
+++ src/app/perfil/page.js
@@ -1,12 +1,12 @@
 import Navbar from "../components/navbar/Navbar"
-import Perfil from "../components/perfil/Perfil"
-import Footer from "../components/footer/Footer"
+
+
 const page = () => {
 
   return (
     <div className="flex  flex-col items-center justify-between ">
       <Navbar />
-      <Perfil />
+
     </div>
   )
 }
